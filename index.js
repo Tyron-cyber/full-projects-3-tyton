bbaf7ee7e6f84d71f4d17e60ba6d8552ac1a51ed
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-01-20 13:43:28 +0100

    Nav Done

--- index.js
+++
@@ -1,6 +0,0 @@
-// https://codemyui.com/mobile-dropdown-menu/
-function myFunction(x) {
-    x.classList.toggle("change");
-    $("#mobile-menu").toggleClass("menu-hidden", 800, "easeOutQuint");
-  };
-  
